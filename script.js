92904832f2884e3985d93ff40943706050bbf289
Mohammed Sadiq K <md.sdq.k@gmail.com>
2022-10-04 12:41:53 +0530

    Added score functionality

--- script.js
+++ script.js
@@ -1,36 +1,41 @@
 let color = {
-	red: '',
+  red: '',
   green: '',
   blue: ''
-}
+};
 
 let initSize = 4;
+let score = 0;
+let highScore = 0;
 
 function createGrid(size) {
-	generateRandomColor();
-  
+  generateRandomColor();
+
   const grid = document.getElementById("grid");
   grid.style["grid-template"] = `repeat(${size}, 1fr) / repeat(${size}, 1fr)`;
   grid.innerHTML = "";
 
-  let diffElem = getRandomInt(0, (size*size) - 1);
-	
-  for(let i = 0; i < size*size; i++) {
+  let diffElem = getRandomInt(0, (size * size) - 1);
+
+  for (let i = 0; i < size * size; i++) {
     const div = document.createElement("div");
     div.id = `item${i}`;
     div.classList.add("item");
     div.style.background = getColor();
-    
+
     div.addEventListener("click", (event) => {
-    	if(event.target.id === `item${diffElem}`) {
-      	createGrid(size + 1);
+      if (event.target.id === `item${diffElem}`) {
+        createGrid(size + 1);
+        updateScore(score + 1);
       }
       else {
-      	grid.classList.add("shake");
-      	setTimeout(() => {
-        	grid.classList.remove("shake");
-        	createGrid(initSize);
+        grid.classList.add("shake");
+        setTimeout(() => {
+          grid.classList.remove("shake");
+          createGrid(initSize);
         }, 850);
+        updateHighScore();
+        initScore();
       }
     });
     grid.appendChild(div);
@@ -41,6 +46,20 @@ function createGrid(size) {
   document.querySelector('body').style.background = getShade(0.1);
 }
 
+function updateScore(updatedScore) {
+  score = updatedScore;
+  const scoreTag = document.getElementById("score");
+  scoreTag.innerHTML = score;
+}
+
+function updateHighScore() {
+  if (score > highScore) {
+    highScore = score;
+    localStorage.setItem("highScore", highScore);
+    renderHighScore();
+  }
+}
+
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -48,23 +67,46 @@ function getRandomInt(min, max) {
 }
 
 function generateRandomColor() {
-	color.red = getRandomInt(0, 255);	
+  color.red = getRandomInt(0, 255);
   color.green = getRandomInt(0, 255);
-	color.blue = getRandomInt(0, 255);
+  color.blue = getRandomInt(0, 255);
 }
 
 function getColor() {
-		return `rgb(${color.red}, ${color.green}, ${color.blue})`;
+  return `rgb(${color.red}, ${color.green}, ${color.blue})`;
 
 }
 
 function getShade(percentage) {
-	return `rgba(${color.red}, ${color.green}, ${color.blue}, ${percentage})`;
+  return `rgba(${color.red}, ${color.green}, ${color.blue}, ${percentage})`;
 }
-  
+
 function startGame() {
   createGrid(initSize);
+  initHighScore();
+  initScore();
+  document.getElementById("scores").hidden = false;
   document.querySelector("footer").hidden = false;
+}
+
+function initScore() {
+  score = 0;
+  updateScore(0);
+}
+
+function initHighScore() {
+  const localHighScore = localStorage.getItem("highScore");
+
+  if (localHighScore) {
+    highScore = localHighScore;
+  }
+
+  renderHighScore();
+}
+
+function renderHighScore() {
+  const highScoreTag = document.getElementById("highScore");
+  highScoreTag.innerHTML = highScore ? highScore : "-";
 }
 
 startGame();
